044c094ac9008407d2dae0e6b336b76a99cb4cb2
Christian de Botton <cdebotton@gmail.com>
2015-01-29 21:24:47 -0500

    Fixing watch task.

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var pruno = module.parent.require('pruno');
 var assign = require('object-assign');
 var browserify = require('browserify');
 var watchify = require('watchify');
-
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
 var plugins = require('gulp-load-plugins')();
-
-var bundle = require('./utils/bundle')(pruno);
-var transform = require('./utils/transform');
+var envify = require('envify/custom');
+var to5ify = require('6to5ify');
+var to5Runtime = require('./utils/to5Runtime');
 var onError = require('./utils/on-error');
+
+var PLUGIN_NAME = 'JsTask';
 
 function JSTask(params) {
   this.params = (params || {});
@@ -37,7 +40,6 @@ JSTask.prototype.enqueue = function(gulp, params) {
   params || (params = {});
 
   var bundler = transform(browserify(params.entry, args), params);
-
   return bundle(gulp, bundler, params);
 };
 
@@ -56,4 +58,52 @@ JSTask.prototype.generateWatcher = function(gulp, params) {
   };
 };
 
-module.exports = JSTask;
+var bundle = function (gulp, bundler, params) {
+  var path = params.dist.split('/');
+  var fileName = path.pop();
+  var dist = path.join('/');
+
+  params || (params = {});
+
+  pruno.notify(PLUGIN_NAME, 'Task `' + params.taskName + '` started!')
+
+  return bundler.bundle()
+    .on('error', function (err) {
+      pruno.error(PLUGIN_NAME, err);
+      this.emit('end');
+    })
+    .pipe(source(fileName))
+    .pipe(buffer())
+    .pipe(
+      plugins.if(
+        params.uglify, plugins.uglify()
+      )
+    )
+    .pipe(
+      plugins.if(
+        params['source-maps'], plugins.sourcemaps.init({loadMaps: true})
+      )
+    )
+    .pipe(
+      plugins.if(
+        params['source-maps'], plugins.sourcemaps.write()
+      )
+    )
+    .pipe(gulp.dest(dist));
+};
+
+function transform(bundler, params) {
+  if (params.runtime) {
+    bundler.transform(to5Runtime);
+  }
+
+  bundler.transform(envify({NODE_ENV: 'development'}));
+
+  if (params.es6 || params.harmony || params.react) {
+    bundler.transform(to5ify);
+  }
+
+  return bundler;
+}
+
+module.exports = pruno.extend(JSTask);
---
+++ utils/to5Runtime.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var through = require('through');
+
+function to5Runtime(file) {
+  var data = '';
+  var header = 'require("pruno-js/node_modules/6to5ify/node_modules/6to5/polyfill");';
+  var stream = through(write, end);
+
+  function write(buf) {
+    data += buf;
+  }
+
+  function end() {
+    data = header + data;
+    stream.queue(data);
+    stream.queue(null);
+  }
+
+  return stream;
+}
+
+module.exports = to5Runtime;
